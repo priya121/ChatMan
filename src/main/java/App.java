c57371d3e9495511d118e8cfa0fc5f42e09a25f9
priya121 <priya121@github.com>
2016-03-04 11:10:04 +0000

    Added App class with main method.

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        String IPAddress = "localhost";
+        int port = 44444;
+        IOConsole console = new EchoConsole(System.in, System.out);
+        Server server = new Server(port, console);
+        Client client = new Client(console);
+
+
+        if (args[0].equals("in")) try {
+            ConnectionSocket realSocket = new RealSocket(new Socket(IPAddress, port));
+            OutputStreamCreator outputStreamCreator = new RealOutputStreamCreator();
+            client.writeDataOut(realSocket, outputStreamCreator);
+        } catch (IOException e) {
+            console.showOutput("Invalid input");
+            e.printStackTrace();
+        }
+        else if (args[0].equals("out"))
+            server.readDataIn();
+    }
+}
+
